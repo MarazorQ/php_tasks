d4f0b184618e1b53bd9748b5f031c75d18a519c1
Artem Shkuratov <shuratov2001@mail.ru>
2020-10-16 15:03:37 +0300

    Chapter 1 , task1

---
+++ Chapter1/task1.php
@@ -0,0 +1,29 @@
+<?php 
+	//
+	function find_the_number_of_digits($number){
+		$length = amount($number);
+		$counter = 0;
+        for ($i = 0; $i < $length; $i++) {
+        	if (($number % 10) < 5){
+        		$counter++;;
+        	}
+        	$number /= 10;
+        }
+       echo "Количество цифр в числе меньших 5: $counter";
+	}
+	//поиск общего кол-ва цифр в числе
+	function amount($number){
+		$length = (int) log10($number)+1;
+		return $length;
+	}
+
+	if (isset($_POST['number'])){
+		$N = htmlentities($_POST['number']);
+		find_the_number_of_digits($N);
+	}
+	
+?>
+<form method="POST">
+ 	<input type="text" name="number" placeholder="Введите натурально число..">
+ 	<input type="submit" value="send">	
+</form>
