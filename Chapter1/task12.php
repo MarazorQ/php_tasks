e57b9555b1cc17ceaf5637cff100a17b6fc57367
Artem Shkuratov <shuratov2001@mail.ru>
2020-10-29 14:29:18 +0200

    Chapter 1, task 12

---
+++ Chapter1/task12.php
@@ -0,0 +1,37 @@
+<?php
+	
+	Class WorkWithNumber{
+
+		function __construct($number){
+			$this ->get_number($number);
+		}
+
+		private function get_number($number){
+			if ($this ->simplicity_check($number)){
+				echo "Простое";
+			}else{
+				echo "Составное";
+			}
+		}
+
+		private function simplicity_check($number){
+			$result = true;
+
+			if ($number > 1){
+				for ($i = 2; $i < $number; $i++){
+					if ($number % $i == 0){
+						$result = false;
+						break; 
+					}
+				}
+			}else{
+				$result = false;
+			}
+			return $result;
+		}
+
+	}
+
+	$N = new WorkWithNumber(10);
+	
+?>
