07cab148f575c2e5d0651039c5bfbed454f31dd6
Artem Shkuratov <shuratov2001@mail.ru>
2020-11-10 08:38:49 +0200

    Chapte 2, task 2 revision

--- Tasks/Chapter2/task1.php
+++ Tasks/Chapter2/task1.php
@@ -1,44 +1,57 @@
 <?php
 
-	Class WorkWithArr{
-
-		private function filling_arr($N){
-			for ($i = 0; $i < $N; $i++){
-				$arr[$i] = rand(-10,100);
-				echo '   '."  $arr[$i]   ".'   '; 
-			}
-			echo '</br>';
-
-			for ($i = 0; $i < count($arr); $i++){
-				if ($arr[$i] < 0){
-					break;
-				}
-				echo ' '." $arr[$i] " . ' ';
-			}
-			
+	function fill_arr($size){
+		for ($i = 0; $i < $size; $i++){
+			$arr[$i] = rand(-5,5); 
 		}
-
-		private function get_all_elements_up_to_the_first_negative($arr){
-			for ($i = 0; $i < count($arr); $i++){
-				if ($arr[$i] < 0){
-					break;
-				}
-				echo ' '." arr[i] " . ' ';
-			}
-		}
-
-		private function amount_after_the_last_item($arr){
-			for ($i = 0)
-
-		}
-
-		public function main($N){
-			$this ->filling_arr($N);
-		}
-
+		return $arr;
 	}
 
-	$N = new WorkWithArr();
-	$N -> main(10);
+	function output_arr($arr,$flag){
+		echo " Исходный массив : ";
 
+		switch ($flag) {
+			case '1':
+				foreach ($arr as $key) {
+					echo "  $key  ";
+				}
+				break;
+			
+			case '2':
+				print_r($arr);
+				break;
+		}
+		echo '</br>';
+	}
+
+	function amount_after_the_last_item($arr){
+		$sum = 0;
+
+		for ($i = 0; $i < count($arr); $i++){
+			$sum += $arr[$i];
+
+			if ($arr[$i] < 0){
+				$sum = 0;
+			}
+		}
+		echo " Сумма элеметнов после последнего отрицательного элемента : $sum </br>";
+	}
+
+	function get_all_elements_up_to_the_first_negative($arr){
+		$counter = 0;
+
+		for ($i = 0; $i < count($arr); $i++){
+			if ($arr[$i] < 0){
+				echo "Количество элементов предшествующих первому отрицательному : $counter </br>";
+				break;
+			}
+			$counter++;
+		}
+	}
+
+	$massive = fill_arr(20);
+	output_arr($massive,1);// Второй входной параметр 1 или 2. Это способы вывода , 1- обычный вывод , 2 - prin_r
+	get_all_elements_up_to_the_first_negative($massive);
+	amount_after_the_last_item($massive);
+	
 ?>
